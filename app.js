b406086b48afd062b3d16ed3382a616aa97a9f19
mokumoku-chan <maho.kumo1997@gmail.com>
2022-04-18 00:50:58 +0900

    Studing this

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 $(function(){
-  $('.box1').on('click',function(){
-    $('.box1').addClass('box1-ext');
+  $('.box1').on('click', function(){
+    $(this).slideUp();
   });
-
-  $('.box1').mouseover(function(){
-    $('.box1').addClass('box2-ext');
-  });
-
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
-  });
-});
+})
